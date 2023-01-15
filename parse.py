1338df05b5122e947399c96d2d36bb2f9738f31e
dAtiuyy <49157326+dAtiuyy@users.noreply.github.com>
2023-01-15 16:55:07 +0200

    w

--- parse.py
+++ parse.py
@@ -1,20 +1,13 @@
 import RC4Decrypt
 from Cryptodome.Cipher import ARC4
-import struct
-from RC4aa import RC4
 
 def parsing(data, port, origin):
-        clientSendKey = RC4(bytearray.fromhex("BA15DE"))
-        clientReceiveKey = RC4(bytearray.fromhex("612a806cac78114ba5013cb531"))
-        serverSendKey = RC4(bytearray.fromhex("612a806cac78114ba5013cb531"))
-        serverReceiveKey = RC4(bytearray.fromhex("BA15DE"))
         if origin == 'server':
                 return
+        #print("[{}({})]{}".format(origin, port, int.from_bytes(data, "big")))
         decipher = RC4Decrypt.Decipher(key1=bytearray.fromhex("612a806cac78114ba5013cb531"))
         decrypted_data = decipher.decrypt(data)
-        #print("[{}({})]{}".format(origin, port, decrypted_data[4:6]))
-        print("[{}({})]{}".format(origin, port, clientReceiveKey.decrypt(data[5:])))
-
+        print("[{}({})]{}".format(origin, port, decrypted_data))
 
 def parseHeader(header, port, origin):
         if origin == 'server':
